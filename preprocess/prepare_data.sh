44307f2ec42bce2d11da239e485ddca393888c49
sj1071-cell <shenjun@stu.ahu.edu.cn>
2025-09-02 08:56:35 +0800

    Update prepare_data.sh

--- preprocess/prepare_data.sh
+++ preprocess/prepare_data.sh
@@ -2,5 +2,5 @@ python get_amr_dict.py --dataset_name=Causal-TimeBank
 python get_amr_triple.py --dataset_name=Causal-TimeBank
 python get_align_data.py --dataset_name=Causal-TimeBank
 
-python split_doc_cv.py
+python split_topic.py
 python build_graph_cv.py
